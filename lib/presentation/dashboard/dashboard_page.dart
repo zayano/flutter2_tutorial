530469096a54e2e9a04f0711f58e16487eb3144f
Reza Aditya Sutrino <adityareza46@gmail.com>
2022-07-13 11:33:56 +0700

    add dashboard_page

---
+++ lib/presentation/dashboard/dashboard_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class DashboardPage extends StatelessWidget {
+  static final String TAG = '/dashboard_page';
+  const DashboardPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
