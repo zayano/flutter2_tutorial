09547837afe5845bf30fb66b0ce61aaffbe38b02
Reza Aditya Sutrino <adityareza46@gmail.com>
2022-07-13 11:12:12 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:flutter2_tutorial/presentation/app_widget.dart';
+
+void main() {
+  runApp(const AppWidget());
+}
---
+++ lib/presentation/app_widget.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter2_tutorial/presentation/home/home_page.dart';
+
+class AppWidget extends StatelessWidget {
+  const AppWidget({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
