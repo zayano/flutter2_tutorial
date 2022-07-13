cd302e6e325542c8e9c712da49d9d44aa7348b18
Reza Aditya Sutrino <adityareza46@gmail.com>
2022-07-13 11:33:34 +0700

    update app_widget

--- lib/presentation/app_widget.dart
+++ lib/presentation/app_widget.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter2_tutorial/presentation/home/home_page.dart';
+import 'package:flutter2_tutorial/router/routes.dart';
+import 'package:get/get.dart';
 
 class AppWidget extends StatelessWidget {
   const AppWidget({Key? key}) : super(key: key);
@@ -7,8 +9,8 @@ class AppWidget extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
+    return GetMaterialApp(
+      getPages: Routes.getPages,
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
